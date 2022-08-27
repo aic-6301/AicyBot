7e599741b1572ecc1f63f49c773f9b4bf54672b1
aicy <rx435423@gmail.com>
2022-08-28 01:47:39 +0900

    userinfo作成

---
+++ cogs/user.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from datetime import timedelta, timezone
+
+
+class User(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["ui", "user"])
+    async def userinfo(self, ctx, user: discord.User = None):
+        if user == None:
+            user = ctx.author
+        member = ctx.guild.get_member(user.id)
+        if member is not None:
+            user = member
+        embed = discord.Embed(title=f'{user}の詳細', description=f'{user}の詳しい情報が載っています。', color=user.color)
+        embed.add_field(name='名前', value=user)
+        embed.add_field(name='Botかどうか', value=user.bot)
+        embed.add_field(name='ID', value=user.id)
+        embed.set_thumbnail(url=user.avatar)
+        if member is not None:
+            joined_at = member.joined_at.astimezone(timezone(timedelta(hours=9))).strftime(
+                "%Y/%m/%d %H:%M:%S"
+            )
+            embed.add_field(name="サーバー参加日", value=joined_at, inline=False)
+        created_at = user.created_at.astimezone(timezone(timedelta(hours=9))).strftime(
+        "%Y/%m/%d %H:%M:%S"
+        )
+        embed.add_field(name="アカウント作成日", value=created_at, inline=False)
+        await ctx.reply(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(User(bot))
