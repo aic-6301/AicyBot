2278d52ccfbf4a7d2a810d05809ece5b95df5a84
aicy <rx435423@gmail.com>
2022-08-27 20:17:49 +0900

    aaaaaaa

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -14,8 +14,8 @@ class Botinfo(commands.Cog):
             await ctx.send(f'pong!:ping_pong: {round(self.bot.latency * 1000)}ms')
     
     @ping.command()
-    async def site(self, ctx, message):
-        url = requests.get(f"https://aic-group.sytes.net/api/get/ping/?type=json?ip={message}")
+    async def site(self, ctx, url):
+        url = requests.get(f"https://aic-group.sytes.net/api/get/ping/?type=json?ip="+url)
         text = url.text
         data = json.loads(text)
         await ctx.send('ping値' + str(data['ping']) + f'\n' + 'ping先:' + (data['domain']))
