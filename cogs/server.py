de68c3060a7c5da3de1e50b819752fe57af5e847
aicy <rx435423@gmail.com>
2022-08-28 02:26:14 +0900

    serverinfo追加

---
+++ cogs/server.py
@@ -0,0 +1,26 @@
+import discord
+from datetime import timezone, timedelta
+from discord.ext import commands
+
+
+class Server(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def serverinfo(self, ctx, guild: int =None):
+        if guild == None:
+            guild = ctx.guild
+        roles = [role for role in guild.roles]
+        text_channels = [text_channels for text_channels in guild.text_channels]
+        embed = discord.Embed(title=f'サーバー情報 - {guild.name}', timestamp=ctx.message.created_at)
+        embed.add_field(name='サーバー名', value=f'{guild.name}',inline=False)
+        embed.add_field(name='チャンネル数', value=f'{len(text_channels)}', inline=False)
+        embed.add_field(name='ロール数', value=f'{len(roles)}', inline=False)
+        embed.add_field(name='サーバーブースト数', value=guild.premium_subscription_count, inline=False)
+        embed.add_field(name='メンバー数', value=f'{guild.member_count}', inline=False)
+        embed.add_field(name='サーバー作成日', value=f'{guild.created_at}', inline=False)
+        await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Server(bot))
