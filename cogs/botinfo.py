e0a0211c688fbbb606c013996afb6e69328f9a4d
aicy <rx435423@gmail.com>
2022-08-29 21:41:39 +0900

    ping修正

--- cogs/botinfo.py
+++ cogs/botinfo.py
@@ -9,25 +9,26 @@ class Botinfo(commands.Cog):
         self.bot = bot
 
     @commands.group()
-    async def ping(self, ctx):
+    async def ping(self, ctx, url=None):
         if ctx.invoked_subcommand is None:
-            await ctx.send(f'pong!:ping_pong: {round(self.bot.latency * 1000)}ms')
-    
-    @ping.command()
-    async def site(self, ctx, url=None):
-        if url is None:
-            url = requests.get(f"https://aic-group.net/api/get/ping/?type=json")
-            text = url.text
-            data = json.loads(text)
-            await ctx.send('ping値' + str(data['ping']) + f'\n' + 'ping先:' + (data['domain']))
-        else:
-            try:
-                url = requests.get(f"https://aic-group.net/api/get/ping/?type=json&ip="+url)
+            if url is None:
+                url = requests.get(f"https://api.aic-group.net/get/ping?type=json")
                 text = url.text
                 data = json.loads(text)
                 await ctx.send('ping値' + str(data['ping']) + f'\n' + 'ping先:' + (data['domain']))
-            except:
-                await ctx.send('URLが存在しません。別のURLを試してみて下さい')
+            else:
+                try:
+                    url = requests.get(f"https://api.aic-group.net/get/ping?type=json&ip="+url)
+                    text = url.text
+                    data = json.loads(text)
+                    await ctx.send('ping値' + str(data['ping']) + f'\n' + 'ping先:' + (data['domain']))
+                except:
+                    await ctx.send('URLが存在しません。別のURLを試してみて下さい')
+    
+    @ping.command()
+    async def websocket(self, ctx, url=None):
+        await ctx.send(f'pong!:ping_pong: {round(self.bot.latency * 1000)}ms')
+        
 
 async def setup(bot):
     await bot.add_cog(Botinfo(bot))
