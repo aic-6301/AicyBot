1be030f2991139274fca879e9ff1b58466e56df1
aicy <rx435423@gmail.com>
2022-08-29 22:00:55 +0900

    status追加

--- cogs/info.py
+++ cogs/info.py
@@ -28,7 +28,20 @@ class Botinfo(commands.Cog):
     @ping.command()
     async def websocket(self, ctx, url=None):
         await ctx.send(f'pong!:ping_pong: {round(self.bot.latency * 1000)}ms')
-        
+    
+    @commands.command()
+    async def status(self, ctx):
+        if ctx.invoked_subcommand is None:
+            url = requests.get("https://api.aic-group.net/get/status")
+            text = url.text
+            data = json.loads(text)
+            embed = discord.Embed(title='ステーサス', description='サーバーのステーサス情報です')
+            embed.add_field(name='メインサイト', value=(data['MainSite']))
+            embed.add_field(name='ブログサイト', value=(data['BlogSite']))
+            embed.add_field(name='Wikiサイト', value=(data['WikiSite']))
+            embed.add_field(name='メディアサイト', value=(data['MediaSite']))
+            embed.add_field(name='API', value=(data['AicyAPI']))
+            await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Botinfo(bot))
