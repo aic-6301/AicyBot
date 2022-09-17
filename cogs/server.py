d33831b54947f2b2051767b09150539743208c76
aicy <rx435423@gmail.com>
2022-09-17 09:14:39 +0900

    mc管理機能追加

--- cogs/server.py
+++ cogs/server.py
@@ -1,7 +1,11 @@
 import discord
 from datetime import timezone, timedelta
 from discord.ext import commands
+from mcpi import minecraft
+import os
+import time
 
+mc = minecraft.Minecraft.create("mc.aic-group.net", 25565)
 
 class Server(commands.Cog):
     def __init__(self, bot):
@@ -22,6 +26,27 @@ class Server(commands.Cog):
         embed.add_field(name='サーバー作成日', value=guild.created_at.strftime("%Y/%m/%d %H:%M:%S"), inline=False)
         embed.set_thumbnail(url=guild.icon.url)
         await ctx.send(embed=embed)
+    @commands.command()
+    async def start(self, ctx):
+        if self.bot.guild == True:
+            if os.system('systemctl is-active mc') == 'active':
+                await ctx.reply('サーバーはすでに起動済みです!')
+            else :
+                await ctx.send('サーバーの起動を開始します')
+                os.system('systemctl start mc')
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def stop(self, ctx):
+        if self.bot.guild == True:
+            if os.system('systemctl is-active mc') == 'active':
+                await ctx.reply('1分後にサーバーを停止します')
+                mc.postToChat("1分後にサーバーを停止します")
+                time.sleep(60)
+                mc.postToChat("サーバーを停止します")
+                os.system('systemctl stop mc')
+            else :
+                await ctx.reply('すでにサーバーは停止されています!')
+            
 
 async def setup(bot):
     await bot.add_cog(Server(bot))
