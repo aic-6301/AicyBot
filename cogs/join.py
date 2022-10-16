771a2957d583182cbfd97582b9c99ac32559db12
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-10-16 23:01:51 +0900

    自己紹介を勧めるように

--- cogs/join.py
+++ cogs/join.py
@@ -18,13 +18,14 @@ class Join(commands.Cog):
                 await self.bot.guild.system_channel.send(embed = embed)
 
     @commands.Cog.listener()
-    async def on_member_update(self, before, after):
+    async def on_member_update(self, before, after, member):
         Member_role = self.bot.guild.get_role(957605646231019540)
         if Member_role not in before.roles and Member_role in after.roles:
             join_jst = after.joined_at + timedelta(hours = 9)
             embed = discord.Embed(title="新規参加", colour=discord.Colour.random(), description="新しいユーザーが認証を終えて参加しました！\n歓迎してあげてね", timestamp=datetime.now())
+            embed.add_field(name='入った人へ', value='<#964090566210121738>で自己紹介をお願いします。')
             embed.set_author(name=after.display_name, icon_url=after.display_avatar.url)
             embed.add_field(name="サーバー参加日時", value=join_jst.strftime("%Y/%m/%d, %H:%M:%S"), inline=False)
-            await self.bot.guild.system_channel.send(embed = embed)
+            await self.bot.guild.system_channel.send(content=member.mention,embed = embed)
 async def setup(bot):
     await bot.add_cog(Join(bot))
