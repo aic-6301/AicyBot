eb9e3aec99422c89b107e4b9bdd0a4c5e7718af4
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-12-17 09:56:11 +0900

    場所違うやんけ

--- cogs/api.py
+++ cogs/api.py
@@ -68,13 +68,13 @@ class api(commands.Cog):
                     embed.add_field(name='AicyWiki', value=status)
                 if (data['AicyMedia']['status']):
                     status = ':white_check_mark:[アクセス可能'
-                    embed.add_field(name='メディアサイト', value=status+']('+ data['AicyMedia']['url'] +')')
+                    embed.add_field(name='メディアサイト', value=status+']('+ data['AicyMedia']['url'][0] +')')
                 else:
                     status = ':octagonal_sign:アクセス不可'
                     embed.add_field(name='メディアサイト', value=status)
                 if (data['AicyAPI']['status']):
                     status = ':white_check_mark:[アクセス可能'
-                    embed.add_field(name='AicyAPI', value=status+']('+ data['AicyAPI']['url'][0] +')')
+                    embed.add_field(name='AicyAPI', value=status+']('+ data['AicyAPI']['url'] +')')
                 else:
                     status = ':octagonal_sign:アクセス不可'
                     embed.add_field(name='API', value=status)
