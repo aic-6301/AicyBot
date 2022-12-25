7ce0c03fe94c842c0a55d87794f3c4dd0f1550ee
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-12-25 19:53:04 +0900

    一定の場所から一定の場所にメッセージ転送

--- cogs/global.py
+++ cogs/global.py
@@ -8,6 +8,10 @@ class Global(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        
+        if message.channel.id == 1033496649395347456:
+            if message.author.bot:
+                return
+            await self.bot.get_channel(949560203886604293).send(message.author+' - '+message.content)
+            await message.add_reaction('✅')
 async def setup(bot):
     await bot.add_cog(Global(bot))
