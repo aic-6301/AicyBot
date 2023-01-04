1e0be507172c1917f8eb806ff733f24b407fa134
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2023-01-04 23:45:10 +0900

    娯楽・ゲーム機能 Fixes #5

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import requests
+import json
 from typing import Literal
 
 
@@ -17,5 +18,24 @@ class Fun(commands.Cog):
         with open('data/5000choyen.png', 'wb') as f:
             f.write(url.content)
         await interaction.response.send_message(file=discord.File("data/5000choyen.png"))
+
+    @group.command(name="neko", description="NekoBotAPIを使った画像機能")
+    async def neko(self, interaction: discord.Interaction, type: Literal['neko', 'food', 'coffee'] =None):
+        if type is None:
+            url = requests.get('https://nekobot.xyz/api/image?type=neko')
+            text = url.text
+            data = json.loads(text)
+            em = discord.Embed(color=data['color'])
+            em.set_image(url=data['message'])
+            em.set_footer(text="Powered by NekoBot API", icon_url="https://images.discordapp.net/avatars/310039170792030211/237b80900f1dc974efcd1758701b594d.png")
+            await interaction.response.send_message(embed=em)
+        else:
+            url = requests.get(f'https://nekobot.xyz/api/image?type={type}')
+            text = url.text
+            data = json.loads(text)
+            em = discord.Embed(color=data['color'])
+            em.set_image(url=data['message'])
+            em.set_footer(text="Powered by NekoBot API", icon_url="https://images.discordapp.net/avatars/310039170792030211/237b80900f1dc974efcd1758701b594d.png")
+            await interaction.response.send_message(embed=em)
 async def setup(bot):
     await bot.add_cog(Fun(bot))
