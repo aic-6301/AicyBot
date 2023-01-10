ea9c03a034e604781c0ca25cc09d9b55225b8846
aic-6301 <rx435423@gmail.com>
2023-01-10 13:10:53 +0900

    vc作成機能の改変 Fixes #13

--- cogs/log.py
+++ cogs/log.py
@@ -8,28 +8,31 @@ class log(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message_delete(self, message):
+    async def on_delete(self, message):
         embed = discord.Embed(title="メッセージ削除")
         embed.set_author(name=message.author)
         embed.add_field(name="メッセージ", value=message.content, inline=False)
-        if message.attachments:
-            embed.add_field(name="ファイル", value=message.attachments.url, inline=False)
-        embed.add_field(name="メッセージ送信日", value=datetime.timestamp(message.created_at), inline=False)
+        if message.attachments and message.attachments[0].proxy_url:
+            embed.add_field(name="添付ファイル"
+            , value=message.attachments[0].proxy_url)
+        embed.add_field(name="メッセージ送信日", value=datetime.date(message.created_at), inline=False)
         embed.add_field(name="削除された日", value=datetime.timestamp(datetime.now()), inline=False)
         embed.set_footer(text=f"Aicy -"+ datetime.now().strftime('%Y/%m/%d %H:%M'))
+        await self.bot.get_channel()
     @commands.Cog.listener()
-    async def on_message_edit(self,before, after):
+    async def on_edit(self,before, after):
         embed = discord.Embed(title="メッセージ編集")
-        embed.set_author(name=before.message.author)
-        embed.add_field(name="編集前メッセージ", value=before.message.content, inline=False)
-        if before.message.attachments:
-            embed.add_field(name="ファイル", value=before.message.attachments.url, inline=False)
-        embed.add_field(name="編集後メッセージ", value=after.message.content, inline=False)
-        if before.message.attachments:
-            embed.add_field(name="ファイル", value=after.message.attachments.url, inline=False)
-        embed.add_field(name="メッセージ送信日", value=datetime.timestamp(before.message.created_at), inline=False)
-        embed.add_field(name="編集された日", value=datetime.timestamp(after.message.edited_at), inline=False)
+        embed.set_author(name=before.author)
+        embed.add_field(name="編集前メッセージ", value=before.content, inline=False)
+        if before.attachments:
+            embed.add_field(name="ファイル", value=before.attachments.url, inline=False)
+        embed.add_field(name="編集後メッセージ", value=after.content, inline=False)
+        if before.attachments:
+            embed.add_field(name="ファイル", value=after.attachments.url, inline=False)
+        embed.add_field(name="メッセージ送信日", value=datetime.timestamp(before.created_at), inline=False)
+        embed.add_field(name="編集された日", value=datetime.timestamp(after.edited_at), inline=False)
         embed.set_footer(text=f"Aicy -"+ datetime.now().strftime('%Y/%m/%d %H:%M'))
+        
 
 async def setup(bot):
     await bot.add_cog(log(bot))
