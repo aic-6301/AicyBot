46c3e9108597cb158d5478ba67b85f143bc3ab3d
aic-6301 <rx435423@gmail.com>
2023-01-21 13:02:31 +0900

    書き直し+pingコマンド+エラーの表示

--- cogs/cog.py
+++ cogs/cog.py
@@ -1,134 +1,38 @@
-import discord
-from discord.ext import commands
-import traceback
+import disnake
+from disnake.ext import commands
 
-class test(discord.ui.View):
-    def __init__(self, bot):
-        super().__init__()
-        self.timeout=None
-        self.bot = bot
-
-    @discord.ui.button(label="aaa", emoji="ℹ️", style=discord.ButtonStyle.green)
-    async def test_callback(self, interaction:discord.Interaction, button:discord.Button):
-        await interaction.response.send_message("やあ！")
 
 class Cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-            error_ch = self.bot.get_channel(1033496616130334784)
-            if isinstance(error, discord.ext.commands.errors.MissingPermissions):
-                embed = discord.Embed(title=":x: 失敗 -MissingPermissions", description=f"実行者の必要な権限が無いため実行出来ません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
-                embed.set_footer(text="お困りの場合はBot管理者までお問い合わせください")
-                await ctx.send(embed=embed)
-                e = discord.Embed(title='エラー情報', description='',  timestamp=ctx.message.created_at, color=discord.Colour.red())
-                e.add_field(name='エラーが発生したサーバー', value=ctx.message.guild.name, inline=False)
-                e.add_field(name='エラーが発生したチャンネル', value=ctx.message.channel.name, inline=False)
-                e.add_field(name='エラーid', value=ctx.message.id, inline=False)
-                e.add_field(name='エラーが発生したコマンド', value=ctx.message.content, inline=False)
-                e.add_field(name='エラーを発生させた人', value=ctx.author, inline=False)
-                e.add_field(name='エラー内容', value=error, inline=False)
-                await error_ch.send(embed=e)
-            elif isinstance(error, discord.ext.commands.errors.NotOwner):
-                embed = discord.Embed(title=":x: 失敗 -MissingPermissions", description=f"実行者の必要な権限が無いため実行出来ません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
-                embed.set_footer(text="お困りの場合はBot管理者までお問い合わせください")
-                await ctx.send(embed=embed)
-                e = discord.Embed(title='エラー情報', description='',  timestamp=ctx.message.created_at, color=discord.Colour.red())
-                e.add_field(name='エラーが発生したサーバー', value=ctx.message.guild.name, inline=False)
-                e.add_field(name='エラーが発生したチャンネル', value=ctx.message.channel.name, inline=False)
-                e.add_field(name='エラーid', value=ctx.message.id, inline=False)
-                e.add_field(name='エラーが発生したコマンド', value=ctx.message.content, inline=False)
-                e.add_field(name='エラーを発生させた人', value=ctx.author, inline=False)
-                e.add_field(name='エラー内容', value=error, inline=False)
-                await error_ch.send(embed=e)
-            elif isinstance(error, discord.ext.commands.errors.BotMissingPermissions):
-                embed = discord.Embed(title=":x: 失敗 -BotMissingPermissions", description=f"Botの必要な権限が無いため実行出来ません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
-                embed.set_footer(text="お困りの場合はBot管理者までお問い合わせください")
-                await ctx.send(embed=embed)
-                e = discord.Embed(title='エラー情報', description='',  timestamp=ctx.message.created_at, color=discord.Colour.red())
-                e.add_field(name='エラーが発生したサーバー', value=ctx.message.guild.name, inline=False)
-                e.add_field(name='エラーが発生したチャンネル', value=ctx.message.channel.name, inline=False)
-                e.add_field(name='エラーid', value=ctx.message.id, inline=False)
-                e.add_field(name='エラーが発生したコマンド', value=ctx.message.content, inline=False)
-                e.add_field(name='エラーを発生させた人', value=ctx.author, inline=False)
-                e.add_field(name='エラー内容', value=error, inline=False)
-                await error_ch.send(embed=e)
-            elif isinstance(error, discord.ext.commands.errors.CommandNotFound):
-                embed = discord.Embed(title=":x: 失敗 -CommandNotFound", description=f"不明なコマンドもしくは現在使用不可能なコマンドです。", timestamp=ctx.message.created_at, color=discord.Colour.red())
-                embed.set_footer(text="お困りの場合はBot管理者までお問い合わせください")
-                await ctx.send(embed=embed)
-                e = discord.Embed(title='エラー情報', description='',  timestamp=ctx.message.created_at, color=discord.Colour.red())
-                e.add_field(name='エラーが発生したサーバー', value=ctx.message.guild.name, inline=False)
-                e.add_field(name='エラーが発生したチャンネル', value=ctx.message.channel.name, inline=False)
-                e.add_field(name='エラーid', value=ctx.message.id, inline=False)
-                e.add_field(name='エラーが発生したコマンド', value=ctx.message.content, inline=False)
-                e.add_field(name='エラーを発生させた人', value=ctx.author, inline=False)
-                e.add_field(name='エラー内容', value=error, inline=False)
-                await error_ch.send(embed=e)
-            elif isinstance(error, discord.ext.commands.errors.MemberNotFound):
-                embed = discord.Embed(title=":x: 失敗 -MemberNotFound", description=f"指定されたメンバーが見つかりません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
-                embed.set_footer(text="お困りの場合はBot管理者までお問い合わせください")
-                await ctx.send(embed=embed)
-                e = discord.Embed(title='エラー情報', description='',  timestamp=ctx.message.created_at, color=discord.Colour.red())
-                e.add_field(name='エラーが発生したサーバー', value=ctx.message.guild.name, inline=False)
-                e.add_field(name='エラーが発生したチャンネル', value=ctx.message.channel.name, inline=False)
-                e.add_field(name='エラーid', value=ctx.message.id, inline=False)
-                e.add_field(name='エラーが発生したコマンド', value=ctx.message.content, inline=False)
-                e.add_field(name='エラーを発生させた人', value=ctx.author, inline=False)
-                e.add_field(name='エラー内容', value=error, inline=False)
-                await error_ch.send(embed=e)
-            elif isinstance(error, discord.ext.commands.errors.BadArgument):
-                embed = discord.Embed(title=":x: 失敗 -BadArgument", description=f"指定された引数がエラーを起こしているため実行出来ません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
-                embed.set_footer(text="お困りの場合はBot管理者までお問い合わせください")
-                await ctx.send(embed=embed)
-                e = discord.Embed(title='エラー情報', description='',  timestamp=ctx.message.created_at, color=discord.Colour.red())
-                e.add_field(name='エラーが発生したサーバー', value=ctx.message.guild.name, inline=False)
-                e.add_field(name='エラーが発生したチャンネル', value=ctx.message.channel.name, inline=False)
-                e.add_field(name='エラーid', value=ctx.message.id, inline=False)
-                e.add_field(name='エラーが発生したコマンド', value=ctx.message.content, inline=False)
-                e.add_field(name='エラーを発生させた人', value=ctx.author, inline=False)
-                e.add_field(name='エラー内容', value=error, inline=False)
-                await error_ch.send(embed=e)
-            elif isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-                embed = discord.Embed(title=":x: 失敗 -BadArgument", description=f"指定された引数が足りないため実行出来ません。", timestamp=ctx.message.created_at, color=discord.Colour.red())
-                embed.set_footer(text="お困りの場合はBot管理者までお問い合わせください")
-                await ctx.send(embed=embed)
-                e = discord.Embed(title='エラー情報', description='',  timestamp=ctx.message.created_at, color=discord.Colour.red())
-                e.add_field(name='エラーが発生したサーバー', value=ctx.message.guild.name, inline=False)
-                e.add_field(name='エラーが発生したチャンネル', value=ctx.message.channel.name, inline=False)
-                e.add_field(name='エラーid', value=ctx.message.id, inline=False)
-                e.add_field(name='エラーが発生したコマンド', value=ctx.message.content, inline=False)
-                e.add_field(name='エラーを発生させた人', value=ctx.author, inline=False)
-                e.add_field(name='エラー内容', value=error, inline=False)
-                await error_ch.send(embed=e)
-            else:
-                embed = discord.Embed(title=":x: 失敗", description=f'不明なエラーが発生しました', timestamp=ctx.message.created_at, color=discord.Colour.red())
-                embed.add_field(name='お問い合わせの際', value=f'お問い合わせる際にはこちらのidもお持ちください。{ctx.message.id}')
-                embed.set_footer(text="お困りの場合はBot管理者までお問い合わせください")
-                await ctx.send(embed=embed)
-                e = discord.Embed(title='エラー情報', description='',  timestamp=ctx.message.created_at, color=discord.Colour.red())
-                e.add_field(name='エラーが発生したサーバー', value=ctx.message.guild.name, inline=False)
-                e.add_field(name='エラーが発生したチャンネル', value=ctx.message.channel.name, inline=False)
-                e.add_field(name='エラーid', value=ctx.message.id, inline=False)
-                e.add_field(name='エラーが発生したコマンド', value=ctx.message.content, inline=False)
-                e.add_field(name='エラーを発生させた人', value=ctx.author, inline=False)
-                e.add_field(name='エラー内容', value=error, inline=False)
-                await error_ch.send(embed=e)
-                orig_error = getattr(error, "original", error)
-                error_msg  = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-                await error_ch.send('エラー全文')
-                await error_ch.send(error_msg)
-    
-    @commands.command()
-    async def test_button(self, ctx):
-        await ctx.send("button test", view=discord.ui.button(label="aaa", emoji="ℹ️", style=discord.ButtonStyle.green, custom_id='test_button'))
+    async def on_command_error(self, ctx: commands.Context, error) -> None:
+        if isinstance(error, commands.errors.CommandNotFound):
+            embed = disnake.Embed(
+            title="❌コマンドが存在しません。",
+            description="helpを参照してください",
+            color=disnake.Color.red(),
+        )
+        else:
+            embed = disnake.Embed(
+                title="❌コマンド内でエラーが発生しました",
+                description=f"```py\n{(error)}```",
+                color=disnake.Color.red(),
+            )
+        await ctx.send(embed=embed)
+    @commands.Cog.listener()
+    async def on_slash_command_error(self, inter:disnake.AppCmdInter, error:commands.CommandError):
+        embed = disnake.Embed(
+            title="❌コマンド内でエラーが発生しました",
+            description=(error),
+            color=disnake.Color.red(),
+        )
+        if inter.response.is_done():
+            send = inter.channel.send
+        else:
+            send = inter.response.send_message
+        await send(embed=embed)
 
-
-@commands.Cog.listener()
-async def on_interaction(inter:discord.Interaction):
-    if inter
-
-async def setup(bot):
-    await bot.add_cog(Cog(bot))
+def setup(bot):
+    bot.add_cog(Cog(bot))
--- main.py
+++ main.py
@@ -1,102 +1,44 @@
-import discord
-import os
-import traceback
-from discord.ext import commands, tasks
-from discord import app_commands
-import logging
-import requests
-from json import load
-from dotenv import load_dotenv
-import subprocess
-from datetime import datetime
 import asyncio
-import json
-from motor import motor_asyncio as motor
+import logging
+import os
+import sys
+import traceback
+from dotenv import load_dotenv
+import datetime
+from datetime import timezone
+
+
+import disnake
+from disnake.ext import commands
 
 load_dotenv()
-token = os.environ['token']
-db_token = os.environ['db_token']
-dbclient = motor.AsyncIOMotorClient(f"mongodb+srv://aic:{db_token}@aicybot.saxcvlv.mongodb.net/test")
-
-class aicyserer(commands.Bot):
+TOKEN = os.getenv("token")
+class AicyBot(commands.Bot):
     def __init__(self):
         super().__init__(
-            command_prefix='a!',
-            allowed_mentions=discord.AllowedMentions(replied_user=False),
-            intents = discord.Intents.all()
-            )
+            command_prefix="a!",
+            intents=disnake.Intents.all(),
+            help_command=None,
+            command_sync_flags=commands.CommandSyncFlags.all(),
+        )
+        self.start_time: datetime = datetime.datetime.utcnow().replace(tzinfo=timezone.utc)
+
     async def on_ready(self):
-        print(f"{bot.user}でログイン中・・・")
-        await bot.change_presence(activity = discord.Activity(name=f"起動中・・・", type=discord.ActivityType.playing), status='dnd')
+        await self.change_presence(activity=disnake.Game(name="あいしぃーぼっと"), status="online")
         try:
-            with open('config.json', 'r+', encoding='utf-8') as file:
-                bot.config = load(file)
-            print('Loaded json: config.json')
-        except:
+            self.load_extension("jishaku")
+        except Exception:
             traceback.print_exc()
-        try:
-            await bot.load_extension("jishaku")
-            print("Loaded extension: jishaku")
-        except:
-            traceback.print_exc()
-        print("定義中")
-        #VC系
-        bot.vc1 = None
-        bot.vc1_owner = None
-        bot.vc1_dash = None
-        bot.vc1_status = 'Normal'
-        bot.vc2 = None
-        bot.vc2_owner = None
-        bot.vc2_dash = None
-        bot.vc2_status = 'Normal'
-        bot.vc3 = None
-        bot.vc3_owner = None
-        bot.vc3_dash = None
-        bot.vc3_status = 'Normal'
-        bot.vc4 = None
-        bot.vc4_owner = None
-        bot.vc4_dash = None
-        bot.vc4_status = 'Normal'
-        bot.vc5 = None
-        bot.vc5_owner = None
-        bot.vc5_dash = None
-        bot.vc5_status = 'Normal'
-        # サーバー系
-        bot.guild = bot.get_guild(949560203374915605)
-        bot.admin_guild = bot.get_guild(1033496363897475163)
-        bot.vip = bot.guild.get_role(1015602734684184677)
-        bot.everyone = bot.guild.get_role(949560203374915605)
-        bot.boot_log = bot.get_channel(1058005805426814976)
-        bot.owner = bot.get_user(964887498436276305)
-        bot.bot_role = bot.guild.get_role(1063686877829414964)
-        print("定義完了")
-        for file in os.listdir('./cogs'): # cogの中身ロード
-            if file.endswith('.py'):
+        for file in os.listdir("./cogs"): # cogs loader
+            if file.endswith(".py"):
                 try:
-                    await bot.load_extension(f'cogs.{file[:-3]}')
-                    print(f'Loaded cogs: {file[:-3]}')
-                except:
+                    bot.load_extension(f'cogs.{file[:-3]}')
+                    print(f"Loaded cogs: cogs.{file[:-3]}")
+                except Exception:
                     traceback.print_exc()
-        await bot.tree.sync()
-        embed = discord.Embed(title='起動通知', description=f'{bot.user}でログインしました。', color=discord.Colour.from_rgb(160, 106, 84))
-        embed.add_field(name='メンバー数', value=f'{len(bot.users)}人')
-        await bot.boot_log.send(embed=embed)
-        print(f"Login successful. {bot.user}({bot.user.id})")
-        while True:
-            await bot.change_presence(activity = discord.Activity(name=f"Server Running! 最終更新：{datetime.now().strftime('%H:%M')} | メンバー数:{len(bot.users)}", type=discord.ActivityType.streaming), status='idle')
-            await asyncio.sleep(30)
-
-    async def not_found(self, ctx):
-        await ctx.send(f"{ctx.command.name}のサブコマンドがないよ！`a!help {ctx.command.name}`をみてね！")
-bot = aicyserer()
-
-
-
+        print(f"Ready! {self.user.name}({self.user.id})")
+        await self.get_channel(1058005805426814976).send(embed=disnake.Embed(title="起動しました！", description=f"<t:{self.start_time.timestamp():.0f}:R>にAicyBotが起動しました!\n disnake Version: {disnake.__version__}"))
 
 if __name__ == "__main__":
-    print("プログラムを実行しています。")
-    try:
-        bot.run(token)
-    except:
-        traceback.print_exc()
-    print("===========実行完了===========")
+    bot = AicyBot()
+    bot.run(TOKEN)
